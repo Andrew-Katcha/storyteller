030f00e2febc2399826655227ff3b0dd226074a0
Peter Shultz <pshultz@umich.edu>
2017-10-21 10:59:42 -0400

    Upload working

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import flask
+import os 
+from werkzeug.utils import secure_filename
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 app = flask.Flask(__name__)
  
 gloabalObjects = [{'name': 'Test1', 'number': 5342}, {'name': "Test2", 'number': 1112}]
+app.config['UPLOAD_FOLDER'] = dir_path
 
 
 @app.route('/',  methods=['GET', 'POST'])
 def hello_world():
     if flask.request.method == 'POST':
         if 'audioUpload' in flask.request.form:
+            print(flask.request.files)
             print("HELLOW PSETER")
-            print(flask.request.form['audioUpload'])
+            f = flask.request.files['audioUpload']
+            print(f.save(secure_filename(f.filename)))
         
     context = {}
     context['records'] = gloabalObjects
