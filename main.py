3ae973e6e708ff96cb27943cf7972e858511773b
Andrew-Katcha <akatcha@umich.edu>
2017-10-21 10:38:59 -0400

    workng on uploading an audiofile

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ gloabalObjects = [{'name': 'Test1', 'number': 5342}, {'name': "Test2", 'number':
 
 @app.route('/',  methods=['GET', 'POST'])
 def hello_world():
+    if flask.request.method == 'POST':
+        if 'audioUpload' in flask.request.form:
+            print("HELLOW PSETER")
+            print(flask.request.form['audioUpload'])
+        
     context = {}
     context['records'] = gloabalObjects
     return flask.render_template("index.html", **context)
